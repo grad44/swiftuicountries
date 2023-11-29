91d97bb5b9538f1b464807c74bc658c49a1b435a
Josef Gradinger <josef.gradinger@trustbit.tech>
2023-11-29 12:50:14 +0100

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Josef Gradinger on 29.11.23.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
